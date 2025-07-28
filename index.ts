8383f727ca7d6afd107931b26bb1e76fd31f542a
Your Name <your.email@example.com>
2025-07-28 13:59:23 +0300

    added the intergrations to the server side

--- index.ts
+++ index.ts
@@ -9,7 +9,9 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://tasky-client-s65i.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
 
