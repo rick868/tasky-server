8b9ee43d91cc7645e4c2ea34493839241275d396
Your Name <your.email@example.com>
2025-07-28 14:44:19 +0300

    run the build for deployment

--- index.ts
+++ index.ts
@@ -18,6 +18,8 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true }));
 
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/user', userRoutes);
