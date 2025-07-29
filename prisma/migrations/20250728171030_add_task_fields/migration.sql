263527124cf061ff630c333e31d996d22fe9d9b6
Your Name <your.email@example.com>
2025-07-29 14:35:00 +0300

    updated prisma

--- prisma/migrations/20250728171030_add_task_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "tasks" ADD COLUMN     "labels" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ADD COLUMN     "priority" TEXT NOT NULL DEFAULT 'Medium',
-ADD COLUMN     "project" TEXT,
-ALTER COLUMN "description" DROP NOT NULL;
